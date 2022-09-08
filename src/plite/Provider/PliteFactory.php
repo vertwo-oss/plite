049862a4b6935744fc96359b72c765c4e11859ac
Troy Wu <1719707+troywu@users.noreply.github.com>
2022-09-08 15:45:25 +0100

    Removes debugging.

--- src/plite/Provider/PliteFactory.php
+++ src/plite/Provider/PliteFactory.php
@@ -54,7 +54,7 @@ abstract class PliteFactory
     const DEBUG_CONFIG_INFO_WITH_DUMPS = false;
     const DEBUG_DB_CONN                = false;
     const DEBUG_DB_CONN_VERBOSE        = false;
-    const DEBUG_SECRETS_MANAGER        = true;
+    const DEBUG_SECRETS_MANAGER        = false;
     const DEBUG_AWS_CREDS              = false;
 
 
