2b061993ba09f712bd464e8de3bbf339455619a9
Troy Wu <1719707+troywu@users.noreply.github.com>
2022-09-20 12:48:27 +0100

    Creating custom dropzone code.

--- src/web/js/vertwo.js
+++ src/web/js/vertwo.js
@@ -146,3 +146,191 @@ function computePercentage(numerator, denominator, precision = 1) {
     let roundedPct = Math.round(scale * readablePct) / scale;
     return roundedPct;
 }
+
+
+
+function createDropZone($dz, $info, $uploadButton, formDataHandler) {
+    const progressHandler = function (ev) {
+        console.log(ev);
+    };
+    const loadStartHandler = function (ev) {
+        console.log(ev);
+    };
+    const loadHandler = function (ev) {
+        console.log(ev);
+    };
+    const errorHandler = function (ev) {
+        console.log(ev);
+    };
+    const abortHandler = function (ev) {
+        console.log(ev);
+    };
+
+
+
+    var pendingFileList;
+
+
+
+    function uploadFiles(fileList) {
+        var fd = new FormData();
+        $.each(fileList, function (idx, file) {
+            var filename = file['name'];
+            var size = file['size'];
+            var niceSize = fileSize(size);
+            var type = file['type'];
+
+            clog("  >>> " + filename + ": " + niceSize + " (" + type + ")");
+
+            fd.append("file-" + idx, file);
+        });
+
+        if ('undefined' !== typeof formDataHandler) {
+
+
+
+            formDataHandler(fd); // MEAT <==
+
+
+
+        } else {
+            var xhr = new XMLHttpRequest();
+            xhr.upload.addEventListener("loadstart", loadStartHandler, false);
+            xhr.upload.addEventListener("progress", progressHandler, false);
+            xhr.addEventListener("load", loadHandler, false);
+            xhr.addEventListener("error", errorHandler, false);
+            xhr.addEventListener("abort", abortHandler, false);
+
+            var url = "upload"; // FIXME
+            xhr.open("POST", url, true);
+            xhr.send(fd);
+        }
+    }
+
+
+
+    function highlightDropzone($info) {
+        $info.css("border", "4px solid cyan");
+        $info.css("background-color", "rgba(0, 255, 255, 0.25)");
+        $info.find('div:first-child')
+             .html("Let go at any time to drop these files!")
+             .css("color", "white")
+             .css("font-weight", "900");
+    }
+
+
+
+    function resetDropzone($info) {
+        $info.css("border", "4px dashed rgba(0, 255, 255, 0.5)");
+        $info.css("background-color", "rgba(0, 0, 0, 0)");
+        $info.find('div:first-child')
+             .html("Drag and drop files anywhere on this page!")
+             .css("color", "#ccc")
+             .css("font-weight", "100");
+    }
+
+
+
+    function fileSize(size) {
+        var i = Math.floor(Math.log(size) / Math.log(1024));
+        return Math.ceil(size / Math.pow(1024, i)).toFixed(0) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
+    }
+
+
+
+    function updateFileList($info, fileList) {
+        var tableContents = "";
+
+        $.each(fileList, function (idx, file) {
+            var filename = file['name'];
+            var size = file['size'];
+            var niceSize = fileSize(size);
+            var type = file['type'];
+
+            var entry = '<tr class="dz-file-entry">'
+                + '<td>' + '<input type="checkbox" readonly/>' + '</td>'
+                + '<td>' + filename + '</td>'
+                + '<td>' + niceSize + '</td>'
+                + '<td>' + type + '</td>'
+                + '</tr>\n';
+            tableContents = tableContents + entry;
+        });
+
+        var table = '<table class="dz-file-list">\n'
+            + tableContents
+            + '</table>';
+
+        $info.html(table);
+    }
+
+
+
+    $uploadButton.on("click", (ev) => {
+        uploadFiles(pendingFileList);
+    });
+
+
+
+    $dz.on("drop", (ev) => {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        console.log("drop");
+        console.log(ev);
+
+        resetDropzone($info);
+
+        const fileList = ev.dataTransfer.files;
+
+        updateFileList($info, fileList);
+
+        pendingFileList = [];
+
+        $.each(fileList, function (idx, file) {
+            var filename = file['name'];
+            var size = file['size'];
+            var niceSize = fileSize(size);
+            var type = file['type'];
+
+            clog("  -> " + filename + ": " + niceSize + " (" + type + ")");
+
+            pendingFileList.push(file);
+        });
+
+        //uploadFiles(fileList);
+    });
+    $dz.on("dragover", (ev) => {
+        ev.stopPropagation();
+        ev.preventDefault();
+
+        highlightDropzone($info);
+
+        // Style the drag-and-drop as a "copy file" operation.
+        ev.dataTransfer.dropEffect = 'copy';
+    });
+    $dz.on("dragend", (ev) => {
+        ev.stopPropagation();
+        ev.preventDefault();
+        resetDropzone($info);
+    });
+    $dz.on("dragleave", (ev) => {
+        ev.stopPropagation();
+        ev.preventDefault();
+        resetDropzone($info);
+    });
+}
+
+
+
+//
+// NOTE - Use the createDropZone() function like this:
+//
+// const $dz = $(document.body);
+// var $dzInfo = $('#dz-info');
+// var $dzUploadButton = $('#dz-upload-button');
+//
+//
+// createDropZone($dz, $dzInfo, $dzUploadButton);
+//
+// NOTE - Install custom event handlers (for the XHR events) as desired, with additional parameters
+//
