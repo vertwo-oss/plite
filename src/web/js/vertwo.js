39f9d5493f46b68570f173217388245a647d46ca
Troy Wu <1719707+troywu@users.noreply.github.com>
2025-08-01 17:45:07 +0100

    Adds "deployment" to bare-bones web template.

--- src/web/js/vertwo.js
+++ src/web/js/vertwo.js
@@ -1,6 +1,7 @@
 /**
- * Copyright (c) 2012-2022 Troy Wu
+ * Copyright (c) 2012-2025 Troy Wu
  * Copyright (c) 2021-2022 Version2 OÜ
+ *
  * All rights reserved.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
